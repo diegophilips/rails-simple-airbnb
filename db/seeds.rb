d35dc0ca6c14090b4c42ba44dfa7b7021172b8d9
Diego Philips <diego.r.philips@gmail.com>
2020-11-11 16:53:56 +0000

    debut

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Cute room in Clapham',
+  address: '6 gauden road',
+  description: 'This is where I live and it is cute',
+  price_per_night: 1000,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Warehouse in Fulham',
+  address: '2a Byam Street',
+  description: 'Jazz up your life',
+  price_per_night: 89,
+  number_of_guests: 8
+)
+
+Flat.create!(
+  name: 'Cute house in Putney',
+  address: '53 Pleasance Road',
+  description: 'There used to be a cute doggo here',
+  price_per_night: 120,
+  number_of_guests: 3
+)
